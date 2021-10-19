ba7fb5d7a2c730229e126213fda56e5018b79831
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-10-19 13:26:57 -0700

    Error displays

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/components/CoinListItem.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cryptocurrencyappyt.presentation.coin_detail.components
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
